1d56276d399686d6ab64349cfb6612cb137becea
George Marios Koupanis <106868649+xAsmodeus@users.noreply.github.com>
2025-10-26 15:51:00 +0200

    Delete inArray_outArray_import.py

--- inArray_outArray_import.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def transform_array(array):
-    out = np.square(array)
-    out1 = [0] * len(out)
-    for i in range(len(out)):
-        out1[i] = int(out[i])
-    return out1
